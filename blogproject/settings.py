b5820ffc1b1012a87d247b655b7b32bb9cbc1c95
li.jun <81505285@qq.com>
2018-02-08 11:51:56 +0800

    add

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9j%%k#31^(zke65u6evok1o5tiok7!*t!zf-&korc(br-48=vf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost',]
 
 
 # Application definition
